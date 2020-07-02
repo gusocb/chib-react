c9d30d1a56ce49ab3453c64d9db947ae295d4965
gustavo <gustavocruzb@outlook.com>
2020-07-01 21:34:07 -0500

    feature:employers can see available jobs

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,84 +1,89 @@
-import React from 'react';
-import {Link, useHistory} from 'react-router-dom';
-import { useForm } from 'react-hook-form'
-import AuthService from '../../services/auth-services'
-import 'bulma/css/bulma.css';
-import Swal from 'sweetalert2'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import AuthService from "../../services/auth-services";
+import "bulma/css/bulma.css";
+import Swal from "sweetalert2";
 
+const Login = (props) => {
+  const service = new AuthService();
+  const history = useHistory();
+  const { register, errors, handleSubmit } = useForm();
 
-const Login = props => {
-
-    const service = new AuthService();
-    const history = useHistory()
-    const { register,errors, handleSubmit } = useForm()
-  
-
-    const onSubmit = (data) => {
+  const onSubmit = (data) => {
     const phoneNumber = data.phoneNumber;
     const password = data.password;
-    service.login(phoneNumber, password)
-    .then( response => {
-        props.getUser(response)
-    })
-    .then(() => history.push('/profile'))
-    .catch( error => {
-      console.log(error)
-      Swal.fire({
-        title: "Can't login!",
-        text: 'Email or password incorrect',
-        icon: 'error',
-        confirmButtonText: 'Try again'
+    service
+      .login(phoneNumber, password)
+      .then((response) => {
+        props.getUser(response);
       })
-    } )
-  }
+      .then(() => history.push("/profile"))
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          title: "Can't login!",
+          text: "Email or password incorrect",
+          icon: "error",
+          confirmButtonText: "Try again",
+        });
+      });
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className='box'>
-            <div className="field">
-                <label className="label">Teléfono</label>
-                <div className="control">
-                    <input 
-                    className="input" 
-                    type="text" 
-                    name="phoneNumber" 
-                    placeholder='5512131415'
-                    ref={register({
-                        required:true,
-                    })}
-                    />
-                    {errors.username?.type === "required" && <p className='error-form'>Campo requerido</p>}
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)} className="box">
+        <div className="field">
+          <label className="label">Teléfono</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="phoneNumber"
+              placeholder="5512131415"
+              ref={register({
+                required: true,
+              })}
+            />
+            {errors.username?.type === "required" && (
+              <p className="error-form">Campo requerido</p>
+            )}
+          </div>
+        </div>
 
-            <div className="field">
-                <label className="label">Constraseña</label>
-                <div className="control">
-                    <input 
-                    className="input" 
-                    type="password" 
-                    name="password" 
-                    placeholder='Your password here'
-                    ref={register({required:true})}
-                    />
-                    {errors.password && <p className='error-form'>Campo requerido</p>}
-                </div>
-            </div>
+        <div className="field">
+          <label className="label">Contraseña</label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              name="password"
+              placeholder="***************"
+              ref={register({ required: true })}
+            />
+            {errors.password && <p className="error-form">Campo requerido</p>}
+          </div>
+        </div>
 
-            <div className="field">
-            <div className="control">
-                <button className='button is-link is-light' type="submit">Iniciar Sesión</button>
-            </div>
-            </div>
+        <div className="field">
+          <div className="control">
+            <button className="button is-link is-light" type="submit">
+              Iniciar Sesión
+            </button>
+          </div>
+        </div>
 
-            <div className='field'>
-            <div className='control'>
-                <p>¿No tienes cuenta? 
-                    <Link to={"/workers/signup"}> Regístrate</Link>
-                </p>
-            </div>
-            </div>
-        </form>
-    )
+        <div className="field">
+          <div className="control">
+            <p>
+              ¿No tienes cuenta?
+              <Link to={"/workers/signup"}> Regístrate</Link>
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default Login
+export default Login;
--- src/components/workers/Profile.js
+++ src/components/workers/Profile.js
@@ -1,113 +1,146 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useForm } from 'react-hook-form'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
-const Profile = props => {
+const Profile = (props) => {
+  const { register, errors, handleSubmit } = useForm();
 
-    const { register,errors, handleSubmit } = useForm()
+  const [singleUser, updateSingleUser] = useState({
+    name: "",
+    lastName: "",
+    birthday: "",
+    phoneNumber: "",
+    password: "",
+    profession: "",
+    jobDescription: "",
+    pricePerHour: "",
+  });
 
-    const [singleUser, updateSingleUser] = useState({
-        name:'',
-        lastName:'',
-        birthday:'',
-        phoneNumber:'',
-        password:'',
-        profession:'',
-        jobDescription:'',
-        pricePerHour:''
-    })
+  const getInfo = () => {
+    axios
+      .get("http://localhost:5000/api/profile", { withCredentials: true })
+      .then((response) => {
+        updateSingleUser(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const getInfo = () => {
-        axios.get('http://localhost:5000/api/profile', {withCredentials:true})
-        .then(response => {
-            updateSingleUser(response.data)
-        })
-        .catch(err => console.log(err))
-    }
+  useEffect(() => {
+    getInfo();
+  }, []);
 
-    useEffect(()=> {
-        getInfo()
-    }, [])
+  const onSubmit = (data) => {
+    axios
+      .put("http://localhost:5000/api/profile/edit", data, {
+        withCredentials: true,
+      })
+      .then(() => {
+        getInfo();
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const onSubmit = data => {
-        axios.put('http://localhost:5000/api/profile/edit', data, {withCredentials:true})
-        .then(() =>{
-            getInfo()
-        })
-        .catch(err => console.log(err))
-    }
-    if(!singleUser.profession){
-        return(
-            <div className='container'>
-                <p>Bienvenido {singleUser.name} {singleUser.lastName}</p>
-                <p>Completa tu perfil</p>
+  if (!singleUser.profession) {
+    return (
+      <div className="container">
+        <p className="title is-3">
+          Bienvenido {singleUser.name} {singleUser.lastName}
+        </p>
+        <p className="subtitle is-4">Por favor completa tu perfil</p>
 
-                <form onSubmit={handleSubmit(onSubmit)} className='box'>
-
-                    <div className="field">
-                        <label className="label">Oficio</label>
-                        <div className="control">
-                            <input 
-                            className="input" 
-                            type="text" 
-                            name="profession" 
-                            placeholder='Carpintero'
-                            ref={register({
-                                required:true,
-                            })}
-                            />
-                            {errors.profession && <p className='error-form'>Campo requerido</p>}
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <label className="label">Descripción</label>
-                        <div className="control">
-                            <input 
-                            className="input" 
-                            type="text" 
-                            name="jobDescription" 
-                            placeholder='Especialista en ébano'
-                            ref={register({required:true})}
-                            />
-                            {errors.jobDescription && <p className='error-form'>Campo requerido</p>}
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <label className="label">Salario por hora</label>
-                        <div className="control">
-                            <input 
-                            className="input" 
-                            type="text" 
-                            name="pricePerHour" 
-                            placeholder='En MXN'
-                            ref={register({required:true})}
-                            />
-                            {errors.pricePerHour && <p className='error-form'>Campo requerido</p>}
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <div className="control">
-                            <button className='button is-link is-light' type="submit">Actualizar</button>
-                        </div>
-                    </div>
-                </form>
+        <form onSubmit={handleSubmit(onSubmit)} className="box">
+          <div className="field">
+            <label className="label">Oficio</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="profession"
+                placeholder="Carpintero"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.profession && (
+                <p className="error-form">Campo requerido</p>
+              )}
             </div>
-        )
-    }
-    else {
-        return(
-            <div>
-                <p>Bienvenido de vuelta {singleUser.name} {singleUser.lastName}</p>
-                <p>Oficio: {singleUser.profession}</p>
-                <p>Descripción: {singleUser.jobDescription}</p>
-                <p>Cobro por hora: {singleUser.pricePerHour}</p>
+          </div>
+
+          <div className="field">
+            <label className="label">Descripción</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="jobDescription"
+                placeholder="Especialista en ébano"
+                ref={register({ required: true })}
+              />
+              {errors.jobDescription && (
+                <p className="error-form">Campo requerido</p>
+              )}
             </div>
-        )
-    }
-    
-}
+          </div>
+
+          <div className="field">
+            <label className="label">Salario por hora</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="pricePerHour"
+                placeholder="En MXN"
+                ref={register({ required: true })}
+              />
+              {errors.pricePerHour && (
+                <p className="error-form">Campo requerido</p>
+              )}
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <button className="button is-link is-light" type="submit">
+                Actualizar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-96x96">
+                  <img
+                    src="https://bulma.io/images/placeholders/96x96.png"
+                    alt="avatar"
+                  />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-3">Bienvenido de vuelta</p>
+                <p class="title is-4">
+                  {singleUser.name} {singleUser.lastName}
+                </p>
+                <p class="subtitle is-5">{singleUser.profession}</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <p>{singleUser.jobDescription}</p>
+              <p className="title is-6">${singleUser.pricePerHour} la hora.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Profile;
